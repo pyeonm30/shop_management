51bad790778e05c480131291c1ce43398231e2b6
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2023-12-18 12:11:03 +0900

    .
    .

---
+++ Shop/src/Controller/ShopController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+public class ShopController {
+//	System.out.println("[홈하면]");
+//	System.out.println("[1] 로그인 [2] 회원가입 [0]종료");
+//	
+//	System.out.println("[관리자 메뉴]");
+//	System.out.println("[1.아이템관리] [2.카테고리관리] [3.장바구니관리] [4.유저관리] [5.파일 저장] [6.파일 로드] [0.로그아웃] ");
+//	System.out.println("[사용자 메뉴]");
+//	System.out.println("[1] 쇼핑 [2] 주문확인 [3] 탈퇴(주문정보) [0] 로그아웃");
+}
